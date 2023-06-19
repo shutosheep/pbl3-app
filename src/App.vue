6a515530b8590f0b92e8c1ee02a299bf88beee58
Shuto Tamaoka <shuto@shutotamaoka.xyz>
2023-06-19 18:24:35 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div style="height:600px; width:800px">
+    <l-map ref="map" v-model:zoom="zoom" :use-global-leaflet="false" :center="center">
+      <l-tile-layer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        layer-type="base"
+        name="OpenStreetMap"
+      ></l-tile-layer>
+    </l-map>
+  </div>
+</template>
+
+<script>
+import "leaflet/dist/leaflet.css";
+import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
+
+export default {
+  components: {
+    LMap,
+    LTileLayer,
+  },
+  data() {
+    return {
+      zoom: 17,
+      center: [35.003694371110925, 135.94692066374824],
+    };
+  },
+};
+</script>
+
+<style></style>
