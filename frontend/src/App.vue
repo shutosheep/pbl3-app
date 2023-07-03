8897d38bbac46b4f9c4a4910dfc385e3734ccb7e
Shuto Tamaoka <shuto@shutotamaoka.xyz>
2023-07-03 15:30:29 +0900

    feat: show vending machine location with marker

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,26 +5,38 @@
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         layer-type="base"
         name="OpenStreetMap"
-      ></l-tile-layer>
+      />
+      <l-marker :lat-lng="center" />
+      <l-marker v-for='i in vendingmachine' :lat-lng="i.location" ></l-marker>
     </l-map>
   </div>
+  {{ vendingmachine }}
 </template>
 
 <script>
 import "leaflet/dist/leaflet.css";
-import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
+import { LMap, LTileLayer, LMarker } from "@vue-leaflet/vue-leaflet";
+import axios from 'axios';
 
 export default {
   components: {
     LMap,
     LTileLayer,
+    LMarker,
   },
   data() {
     return {
-      zoom: 17,
-      center: [35.003694371110925, 135.94692066374824],
+      zoom: 15,
+      center: [34.982155233542514, 135.9635035902881], // university
+      vendingmachine: [],
     };
   },
+  mounted() {
+    axios
+      .get("http://127.0.0.1:5000/")
+      .then((response) => (this.vendingmachine = response.data))
+      .catch(error => console.log(error))
+  }
 };
 </script>
 
