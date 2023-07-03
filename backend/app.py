e613bef145d79f165c1d918732bd4ed5e8fa98c0
Shuto Tamaoka <shuto@shutotamaoka.xyz>
2023-07-03 15:27:32 +0900

    update: fix CORS error, use location for api

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 import sqlite3
 import pandas as pd
 
@@ -6,18 +7,24 @@ import pandas as pd
 connection = sqlite3.connect("data.db")
 df = pd.read_sql('SELECT * FROM vendingmachine', connection)
 
+# https://zenn.dev/straydog/articles/5ba6d234c7e72f886a5e
+
 app = Flask(__name__)
+CORS(
+    app,
+    supports_credentials=True
+)
 
 @app.route("/", methods=["GET"])
 def get():
     data = []
 
     for i in df.index:
+        location = [float(df.latitude[i]), float(df.longtitude[i])]
         vending_machine = {
             "id": int(df.id[i]),
-            "latitude": float(df.latitude[i]),
-            "longtitude": float(df.longtitude[i]),
-            "cacheless_avilable": int(df.is_cacheless[i])
+            "location": location,
+            "cacheless_avilable": int(df.is_cacheless[i]),
         }
 
         data.append(vending_machine)
