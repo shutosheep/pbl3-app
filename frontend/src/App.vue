d933c9e0766119ba3ea7b9eae4a40d55a60306cf
Shuto Tamaoka <shuto@shutotamaoka.xyz>
2023-07-04 13:43:54 +0900

    feat: add location and menu for clicked vending machine

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,38 +1,42 @@
 <template>
-  <div style="height:600px; width:800px">
-    <l-map ref="map" v-model:zoom="zoom" :use-global-leaflet="false" :center="center">
-      <l-tile-layer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        layer-type="base"
-        name="OpenStreetMap"
-      />
-      <l-marker v-for='i in vendingmachine' @click="clickMarker(i.id)" :lat-lng="i.location" ></l-marker>
-    </l-map>
-  </div>
-  <div>
-    {{ vendingmachine }}
-    <br>
-    {{ menu }}
+  <div class="flex">
+    <div class="map">
+      <l-map ref="map" v-model:zoom="zoom" :use-global-leaflet="false" :center="center">
+        <l-tile-layer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          layer-type="base"
+          name="OpenStreetMap"
+        />
+        <l-marker v-for='i in vendingmachine' @click="clickMarker(i.id)" :lat-lng="i.location" ></l-marker>
+      </l-map>
+    </div>
+    <div class="right">
+      <VendingInfo :menu=menu :location=location />
+    </div>
   </div>
 </template>
 
 <script>
+import "./style/global.scss";
 import "leaflet/dist/leaflet.css";
 import { LMap, LTileLayer, LMarker } from "@vue-leaflet/vue-leaflet";
 import axios from 'axios';
+import VendingInfo from './components/VendingInfo.vue';
 
 export default {
   components: {
     LMap,
     LTileLayer,
     LMarker,
+    VendingInfo,
   },
   data() {
     return {
       zoom: 15,
       center: [34.982155233542514, 135.9635035902881], // university
-      vendingmachine: [],
-      menu: [],
+      vendingmachine: null,
+      menu: null,
+      location: null,
     };
   },
   mounted() {
@@ -48,9 +52,26 @@ export default {
         .get("http://127.0.0.1:5000/menu?id=" + id)
         .then((response) => (this.menu = response.data))
         .catch(error => console.log(error))
+      axios
+        .get("http://127.0.0.1:5000/location?id=" + id)
+        .then((response) => (this.location = response.data))
+        .catch(error => console.log(error))
     }
   },
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.flex {
+  display: flex;
+}
+
+.map {
+  width: 1024px;
+  height: 720px;
+}
+
+.right {
+  margin-left: 1rem;
+}
+</style>
---
+++ frontend/src/components/VendingInfo.vue
@@ -0,0 +1,48 @@
+<template>
+  <h2>Vending machine Info</h2>
+  <div v-if="menu" class="flex">
+    <div class="location">
+      <h3>Location</h3>
+      <p>
+        Latitude:
+        {{ location[0].location[0] }}
+        <br>
+        Longtitude:
+        {{ location[0].location[1] }}
+      </p>
+    </div>
+    <div class="menu">
+      <h3>Menu</h3>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="i in menu">
+            <td>{{ i.name }}</td>
+            <td>{{ i.price }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    menu: { },
+    location: { },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.flex {
+  display: flex;
+  gap: 1rem;
+}
+</style>
