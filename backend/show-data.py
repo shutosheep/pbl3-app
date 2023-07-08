244a4b240c0f658f9f51cd136397e4befa108be1
Shuto Tamaoka <shuto@shutotamaoka.xyz>
2023-07-08 17:58:20 +0900

    chore: update pandas option

--- backend/show-data.py
+++ backend/show-data.py
@@ -1,6 +1,8 @@
 import sqlite3
 import pandas as pd
 
+pd.set_option('display.max_rows', None)
+
 # read database data with pandas
 connection = sqlite3.connect("data.db")
 v_df = pd.read_sql('SELECT * FROM vendingmachine', connection)
